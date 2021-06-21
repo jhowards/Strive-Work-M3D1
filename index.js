1032b5af896e9da55b5a9d842e2f8004f5cd60b7
jhowards <github@jameshoward.uk>
2021-06-21 14:09:52 +0100

    Ex 9 + 10

--- index.js
+++ index.js
@@ -127,62 +127,82 @@ console.log(arraysum([3, 8, 7]));
 Create a function to test whether an array of integers of length 2 contains 1 or a 3. 
 */
 
-/*
-10)
+const arrayints = function (array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 1 || array[i] === 3) {
+      return true;
+    }
+  }
+  return false;
+};
 
-Create a function to test whether an array of integers of length 2 does not contain 1 or a 3
+console.log("<--EX9-->");
+console.log(arrayints([7, 5]));
 
-11)
+// 10)
+// Create a function to test whether an array of integers of length 2 does not contain 1 or a 3
 
-Create a function to find the longest string from a given array of strings.
+const notarrayints = function (array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 1 || array[i] === 3) {
+      return false;
+    }
+  }
+  return true;
+};
 
-12)
+console.log("<--EX10-->");
+console.log(notarrayints([5, 2]));
 
-Create a function to find the types of a given angle.
+// 11)
 
-Types of angles:
-    Acute angle: An angle between 0 and 90 degrees.
-    Right angle: An 90 degree angle.
-    btuse angle: An angle between 90 and 180 degrees.
-    Straight angle: A 180 degree angle.
+// Create a function to find the longest string from a given array of strings.
 
-13)
+// 12)
 
-Create a function to find the index of the greatest element of a given array of integers
+// Create a function to find the types of a given angle.
 
-14)
+// Types of angles:
+//     Acute angle: An angle between 0 and 90 degrees.
+//     Right angle: An 90 degree angle.
+//     btuse angle: An angle between 90 and 180 degrees.
+//     Straight angle: A 180 degree angle.
 
-Create a function to get the largest even number from an array of integers.
+// 13)
 
-15)
+// Create a function to find the index of the greatest element of a given array of integers
 
-Create a function to check from two given integers, whether one is positive and another one is negative.
+// 14)
 
-16)
+// Create a function to get the largest even number from an array of integers.
 
-Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case. 
+// 15)
 
-17)
+// Create a function to check from two given integers, whether one is positive and another one is negative.
 
-Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
+// 16)
 
-18)
+// Create a function to create new string with first 3 characters are in lower case and the others in upper case. If the string length is less than 3 convert all the characters in upper case.
 
-Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
+// 17)
 
-If the number has 3 as a factor, output 'Diego'.
-If the number has 5 as a factor, output 'Riccardo'.
-If the number has 7 as a factor, output 'Stefano'.
-If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
-Examples
-28's factors are 1, 2, 4, 7, 14, 28.
-this would be a simple "Stefano".
-30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
-this would be a "DiegoRiccardo".
-34 has four factors: 1, 2, 17, and 34.
-this would be "34".
+// Create a function to calculate the sum of the two given integers, If the sum is in the range 50..80 return 65 other wise return 80.
 
-19)
-Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
+// 18)
 
-*/
+// Create a function to convert a number to a string, the contents of which depend on the number's factors. Follow next example:
+
+// If the number has 3 as a factor, output 'Diego'.
+// If the number has 5 as a factor, output 'Riccardo'.
+// If the number has 7 as a factor, output 'Stefano'.
+// If the number does not have 3, 5, or 7 as a factor, just pass the number's digits straight through.
+// Examples
+// 28's factors are 1, 2, 4, 7, 14, 28.
+// this would be a simple "Stefano".
+// 30's factors are 1, 2, 3, 5, 6, 10, 15, 30.
+// this would be a "DiegoRiccardo".
+// 34 has four factors: 1, 2, 17, and 34.
+// this would be "34".
+
+// 19)
+// Create a function that given a phrase returns its acronym, like British Broadcasting Corporation returns BBC
